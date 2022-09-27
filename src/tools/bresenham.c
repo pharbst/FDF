627625f52dcffb318e133ce67d0397e18a93aa13
Peter Harbst <pharbst@student.42heilbronn.de>
2022-09-27 18:59:45 +0200

    save commit

--- src/tools/bresenham.c
+++ src/tools/bresenham.c
@@ -6,11 +6,12 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:37:26 by pharbst           #+#    #+#             */
-/*   Updated: 2022/09/27 18:34:01 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/09/27 18:58:37 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fdf.h>
+// #include <fdf.h>
+#include <stdio.h>
 
 void	ft_draw_line(int x0, int y0, int x1, int y1)
 {
@@ -27,23 +28,41 @@ void	ft_draw_line(int x0, int y0, int x1, int y1)
 	{
 		fd = dx;
 		sd = dy;
+		f = fd / 2.0;
+		while (x0 < x1)
+		{
+			printf("%d|%d\n", x0, y0);
+			x0++;
+			f = f - sd;
+			if (f < 0)
+			{
+				y0++;
+				f += fd;
+			}
+		}
 	}
 	else
 	{
 		fd = dy;
 		sd = dx;
-	}
-	f = fd / 2.0;
-	while ()
-	{
-		ft_putpixel(x0, y0);
-		x0++;
-		f = f - sd;
-		if (f < 0)
+		f = fd / 2.0;
+		while (y0 < y1)
 		{
+			printf("%d|%d\n", x0, y0);
 			y0++;
-			f += fd;
+			f = f - sd;
+			if (f < 0)
+			{
+				x0++;
+				f += fd;
+			}
 		}
 	}
 	return ;
+}
+
+int main()
+{
+	ft_draw_line(0, 0, 15, 10);
+	return (0);
 }
