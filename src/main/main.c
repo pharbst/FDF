caf25800ffbcbe27d491dfccf5a918b7b502c5c4
Peter Harbst <pharbst@student.42heilbronn.de>
2022-09-27 17:58:36 +0200

    save commit start with bresenham algo writing

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 23:29:48 by pharbst           #+#    #+#             */
-/*   Updated: 2022/09/23 23:35:12 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/09/27 17:03:56 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,28 @@ void	hook(void *param)
 int	main(void)
 {
 	mlx_t	*mlx;
+	unsigned int	x;
+	unsigned int	y;
 
+	y = 0;
 	mlx = mlx_init(WIDTH, HEIGHT, "FDF", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
 	g_img = mlx_new_image(mlx, 50, 10);
-	memset(g_img->pixels, 255 , g_img->width * g_img->height * sizeof(int));
+	while (y < g_img->height)
+	{
+		x = 0;
+		while (x < g_img->width)
+		{
+			g_img->pixels[x * 4 + y * g_img->width * 4 + 0] = 0x00;		//R
+			g_img->pixels[x * 4 + y * g_img->width * 4 + 1] = 0xFF;		//G
+			g_img->pixels[x * 4 + y * g_img->width * 4 + 2] = 0x00;		//B
+			g_img->pixels[x * 4 + y * g_img->width * 4 + 3] = 0xFF;		//alpha
+			x++;
+		}
+		y++;
+	}
+	// memset(g_img->pixels, 0xFF , g_img->width * g_img->height * sizeof(int));
 	mlx_image_to_window(mlx, g_img, 1300, 800);
 	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
--- src/tools/bresenham.c
+++ src/tools/bresenham.c
@@ -6,69 +6,26 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:37:26 by pharbst           #+#    #+#             */
-/*   Updated: 2022/09/19 18:50:55 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/09/27 17:55:28 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-plotLine(x0, y0, x1, y1)
+void	ft_draw_line(int x0, int y0, int x1, int y1)
 {
-	int	dx;
-	int	sx;
-	int	dy;
-	int	error;
+	int		dx;
+	int		dy;
+	double	sr;
+	int		f;
 
-	dx = abs(x1 - x0)
-	sx = x0 < x1 ? 1 : -1
-	dy = -abs(y1 - y0)
-	sy = y0 < y1 ? 1 : -1
-	error = dx + dy
-
-	while true
-	{
-		plot(x0, y0);
-		if (x0 == x1 && y0 == y1)
-			break ;
-		e2 = 2 * error
-		if (e2 >= dy)
-		{
-			if (x0 == x1)
-				break ;
-			error = error + dy
-			x0 = x0 + sx
-		}
-		if (e2 <= dx)
-		{
-			if (y0 == y1)
-				break ;
-			error = error + dx
-			y0 = y0 + sy
-		}
-	}
-}
-
-
-void	plot_line (int x0, int y0, int x1, int y1)
-{
-	int	dx;
-	int	dy;
-	int	err;
-	int	sx;
-	int	sy;
-	int	e2;
-
-	dx = abs (x1 - x0);
-	sx = x0 < x1 ? 1 : -1;
-	dy = -abs (y1 - y0);
-	sy = y0 < y1 ? 1 : -1; 
-	err = dx + dy, e2; /* error value e_xy */
-	for ()
-	{
-		setPixel (x0,y0);
-		if (x0 == x1 && y0 == y1) break;
-		e2 = 2 * err;
-		if (e2 >= dy) { err += dy; x0 += sx; }
-		if (e2 <= dx) { err += dx; y0 += sy; }
-	}
+	dx = x1 - x0;
+	dy = y1 - y0;
+	if (dx > dy)
+		sr = dx;
+	else
+		sr = dy;
+	f = sr / 2;
+	
+	return ;
 }
