345899bb555984f58f63814f8729271eece144e8
Peter Harbst <pharbst@student.42heilbronn.de>
2022-09-27 18:34:45 +0200

    bresenham algo raw prototype done

--- src/tools/bresenham.c
+++ src/tools/bresenham.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:37:26 by pharbst           #+#    #+#             */
-/*   Updated: 2022/09/27 17:55:28 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/09/27 18:34:01 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,34 @@ void	ft_draw_line(int x0, int y0, int x1, int y1)
 {
 	int		dx;
 	int		dy;
-	double	sr;
-	int		f;
+	int		fd;
+	int		sd;
+	double	f;
+	double	ft;
 
 	dx = x1 - x0;
 	dy = y1 - y0;
 	if (dx > dy)
-		sr = dx;
+	{
+		fd = dx;
+		sd = dy;
+	}
 	else
-		sr = dy;
-	f = sr / 2;
-	
+	{
+		fd = dy;
+		sd = dx;
+	}
+	f = fd / 2.0;
+	while ()
+	{
+		ft_putpixel(x0, y0);
+		x0++;
+		f = f - sd;
+		if (f < 0)
+		{
+			y0++;
+			f += fd;
+		}
+	}
 	return ;
 }
