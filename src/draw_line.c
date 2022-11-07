662e954768f9927060b1e82e3cc27ba1284bea2d
Peter Harbst <pharbst@student.42heilbronn.de>
2022-11-07 18:07:58 +0100

    removed the printfs

--- src/draw_line.c
+++ src/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:37:26 by pharbst           #+#    #+#             */
-/*   Updated: 2022/11/07 17:38:41 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/11/07 18:03:47 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_fdx(t_bre vars, t_a *a)
 {
-	printf("fdx");
+	// printf("fdx");
 	vars.fd = vars.dx;
 	vars.sd = vars.dy;
 	vars.f = vars.fd / 2.0;
 	while (vars.x0 != vars.x1)
 	{
-		printf("%d|%d\n", vars.x0, vars.y0);
+		// printf("%d|%d\n", vars.x0, vars.y0);
 		if (vars.x0 >= 0 && vars.y0 >= 0 && vars.x0 < a->img->width && vars.y0 < a->img->height)
 			ft_set_pixel(vars.x0, vars.y0, a->img, 0XFFFFFFFF);
 		if (vars.x0 < vars.x1)
@@ -41,13 +41,13 @@ void	ft_fdx(t_bre vars, t_a *a)
 
 void	ft_fdy(t_bre vars, t_a *a)
 {
-	printf("fdy\n");
+	// printf("fdy\n");
 	vars.fd = vars.dy;
 	vars.sd = vars.dx;
 	vars.f = vars.fd / 2.0;
 	while (vars.y0 != vars.y1)
 	{
-		printf("%d|%d\n", vars.x0, vars.y0);
+		// printf("%d|%d\n", vars.x0, vars.y0);
 		if ((vars.x0 >= 0 && vars.y0 >= 0) && (vars.x0 < a->img->width && vars.y0 < a->img->height))
 			ft_set_pixel(vars.x0, vars.y0, a->img, 0XFFFFFFFF);
 		if (vars.y0 < vars.y1)
@@ -76,13 +76,13 @@ void	ft_draw_line(unsigned int vector1[2], unsigned int vector2[2], t_a *a)
 	vars.y1 = vector2[1];
 	vars.dx = abs(vars.x1 - vars.x0);
 	vars.dy = abs(vars.y1 - vars.y0);
-	printf("x0 %d| x1 %d| y0 %d| y1 %d\n", vars.x0, vars.x1, vars.y0, vars.y1);
-	printf("dx %d| dy %d\n", vars.dx, vars.dy);
+	// printf("x0 %d| x1 %d| y0 %d| y1 %d\n", vars.x0, vars.x1, vars.y0, vars.y1);
+	// printf("dx %d| dy %d\n", vars.dx, vars.dy);
 	if (vars.dx > vars.dy)
 		ft_fdx(vars, a);
 	else
 		ft_fdy(vars, a);
-	printf("draw_line");
-	fflush(stdout);
+	// printf("draw_line");
+	// fflush(stdout);
 	return ;
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 18:49:51 by pharbst           #+#    #+#             */
-/*   Updated: 2022/11/07 17:39:26 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/11/07 18:05:07 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,12 @@ mlx_image_t	*g_img;
 
 void	hook(void *param)
 {
-	mlx_t	*mlx;
+	t_a	*a;
 
-	mlx = param;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		g_img->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		g_img->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		g_img->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		g_img->instances[0].x += 5;
+	a = (t_a *)param;
+	if (mlx_is_key_down(a->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(a->mlx);
+
 }
 
 int	main(int argc, char **argv)
@@ -47,28 +40,22 @@ int	main(int argc, char **argv)
 	a->map = read_map(a->fd);
 	map_size(a);
 	init_map_coords(a);
-	ft_print_map(a->map);
-	a->gamma = -45;
-	a->beta = 45;
+	// ft_print_map(a->map);
+	a->gamma = -30;
+	a->beta = 30;
 	ft_rotate("yaw", a);
-	ft_print_map(a->map);
+	// ft_print_map(a->map);
 	ft_rotate("pitch", a);
-	ft_print_map(a->map);
+	// ft_print_map(a->map);
 	get_img_coords(a);
-	ft_print_map(a->map);
+	// ft_print_map(a->map);
 	ft_img_calc(a);
-	// ft_draw_line((unsigned int[2]){30, 600}, (unsigned int[2]){10, 492}, a);
-	// ft_set_pixel(10, 492, a->img, 0XFFFF00FF);
-	// ft_set_pixel(57, 492, a->img, 0XFFFF00FF);
-	// ft_set_pixel(104, 492, a->img, 0XFFFF00FF);
-	// ft_set_pixel(57, 42, a->img, 0XFFFF00FF);
-	// ft_set_pixel(500, 500, a->img, 0XFFFF00FF);
 	
 
 
 	mlx_image_to_window(a->mlx, a->mlximg, 0, 0);
-	mlx_loop_hook(a->mlx, &hook, a->mlx);
+	mlx_loop_hook(a->mlx, &hook, a);
 	mlx_loop(a->mlx);
 	mlx_terminate(a->mlx);
-	return (EXIT_SUCCESS);
+	ft_exit(a);
 }
