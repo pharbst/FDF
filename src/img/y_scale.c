1087d7dbc996cefd0fb5b9d5bff440387623edf7
pharbst <pharbst@student.42heilbronn.de>
2022-11-10 17:04:03 +0100

    fixed zoom zoom *= 0.9 | zoom *= 1.1

--- src/img/y_scale.c
+++ src/img/y_scale.c
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   keyaction_zoom.c                                   :+:      :+:    :+:   */
+/*   y_scale.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 01:37:59 by pharbst           #+#    #+#             */
-/*   Updated: 2022/11/10 17:01:43 by pharbst          ###   ########.fr       */
+/*   Created: 2022/11/10 16:24:40 by pharbst           #+#    #+#             */
+/*   Updated: 2022/11/10 16:24:54 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	keyaction_zoom(t_a *a)
+void	y_scale(t_a *a)
 {
-	if (a->keys.add && !a->keys.sub)
-		a->zoom *= 1.1;
-	else if (a->keys.sub && !a->keys.add)
-		a->zoom *= 0.9;
+	t_map	*node;
+
+	node = a->map;
+	while (node)
+	{
+		node->a_y *= a->y;
+		node = node->next;
+	}
 }
